0c5d6e86078790db4b13a6968688b68c35f2b4bb
Juan Carlos <juancarlosjosegan@gmail.com>
2026-08-03 16:21:51 +0800

    feat: update robots.txt to disallow specific query parameters for better SEO control

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/?e=*",
     },
     sitemap: "https://www.imheretravels.com/sitemap.xml",
   };
